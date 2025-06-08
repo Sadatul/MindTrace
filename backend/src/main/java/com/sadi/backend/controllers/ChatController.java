01cdceeef9339c757d819fe8da5d0e72ecce05b6
sadatul <sadatulislamsadi@gmail.com>
2025-06-08 11:45:15 +0600

    Updated chat endpoint to post

--- backend/src/main/java/com/sadi/backend/controllers/ChatController.java
+++ backend/src/main/java/com/sadi/backend/controllers/ChatController.java
@@ -1,9 +1,11 @@
 package com.sadi.backend.controllers;
 
+import com.sadi.backend.dtos.requests.ChatRequest;
 import com.sadi.backend.services.abstractions.ChatService;
 import com.sadi.backend.services.abstractions.LoggingTools;
 import com.sadi.backend.utils.BasicUtils;
 import com.sadi.backend.utils.SecurityUtils;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.chat.client.ChatClient;
@@ -12,10 +14,7 @@ import org.springframework.ai.chat.messages.Message;
 import org.springframework.ai.chat.prompt.SystemPromptTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.Resource;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 
 import java.time.Instant;
@@ -33,29 +32,28 @@ public class ChatController {
     @Value("classpath:/prompts/sysprompt.st")
     private Resource systemPrompt;
 
-    @GetMapping
+    @PostMapping
     public Flux<String> chat(
-            @RequestParam String query,
-            @RequestParam(required = false, defaultValue = "Asia/Dhaka") String zone
-    ) {
-        log.debug("Received query: {}", query);
+            @Valid @RequestBody ChatRequest req
+            ) {
+        log.debug("Received query: {}", req);
         SystemPromptTemplate template = new SystemPromptTemplate(systemPrompt);
-        Message sysPrompt = template.createMessage(Map.of("time", BasicUtils.getISOStringFromZoneIdAndInstant(zone, Instant.now())));
+        Message sysPrompt = template.createMessage(Map.of("time", BasicUtils.getISOStringFromZoneIdAndInstant(req.zone(), Instant.now())));
         StringBuilder response = new StringBuilder();
         String userId = SecurityUtils.getName();
         return chatClient
                 .prompt()
                 .system(sysPrompt.getText())
-                .user(query)
+                .user(req.query())
                 .tools(loggingTools)
-                .toolContext(Map.of("userId", userId, "zone", zone))
+                .toolContext(Map.of("userId", userId, "zone", req.zone()))
                 .advisors(a -> a.param(ChatMemory.CONVERSATION_ID, userId))
                 .stream().chatResponse().doOnNext(
                         chatResponse ->
                             response.append(chatResponse.getResult().getOutput().getText())
                 )
                 .doOnComplete(() ->
-                    chatService.saveChat(query, response.toString(), userId)
+                    chatService.saveChat(req.query(), response.toString(), userId)
                 )
                 .mapNotNull(chatResponse -> chatResponse.getResult().getOutput().getText());
     }
