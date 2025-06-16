e0732029e7b71677f22871a3a8e411b4b5ef8a9e
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-06-16 07:20:21 +0600

    merge completed

--- frontend/app/src/main/java/com/example/frontend/MainActivity.kt
+++ frontend/app/src/main/java/com/example/frontend/MainActivity.kt
@@ -1,10 +1,12 @@
 
 package com.example.frontend
 
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -18,6 +20,7 @@ import com.example.frontend.screens.SetupNavGraph
 import com.example.frontend.ui.theme.FrontendTheme
 
 class MainActivity : ComponentActivity() {
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
