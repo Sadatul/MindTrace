abaf178461f9d3bb239d34d4270f8112b001360b
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-06-20 22:33:05 +0600

    fetched and cached user info, refactored Dashboards, redirected based on auth

---
+++ frontend/app/src/main/java/com/example/frontend/api/SelfUserInfoCache.kt
@@ -0,0 +1,13 @@
+package com.example.frontend.api
+
+object SelfUserInfoCache {
+    private var userInfo: UserInfo? = null
+
+    fun getUserInfo(): UserInfo? {
+        return userInfo
+    }
+
+    fun setUserInfo(userInfo: UserInfo) {
+        this.userInfo = userInfo
+    }
+}
