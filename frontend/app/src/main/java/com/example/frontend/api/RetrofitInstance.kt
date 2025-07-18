b62748243577fc819311d35df9349b3bcbcaa9b5
Irtiaz <irtiazkabir.irtiaz@gmail.com>
2025-07-18 09:49:48 +0600

    made retrofit work with deployed backend

--- frontend/app/src/main/java/com/example/frontend/api/RetrofitInstance.kt
+++ frontend/app/src/main/java/com/example/frontend/api/RetrofitInstance.kt
@@ -6,7 +6,8 @@ import retrofit2.converter.gson.GsonConverterFactory
 object RetrofitInstance {
     val dementiaAPI: DementiaAPI by lazy {
         Retrofit.Builder()
-            .baseUrl("http://10.0.2.2:8080/")
+            .baseUrl("https://mindtrace.pinklifeline.xyz/")
+            //.baseUrl("http://10.0.2.2:8080") // For local testing with Android emulator
             .addConverterFactory(GsonConverterFactory.create())
             .build()
             .create(DementiaAPI::class.java)
